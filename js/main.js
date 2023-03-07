21b332e1ab659ee86bb09b160b4c185fb1c63bdc
TrashmanMT <matheusfms01@outlook.com>
2023-03-07 17:16:14 -0300

    posicao scrollAnimacao

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 import ScrollSuave from "./modules/scroll-suave.js";
-import initAnimaScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/animacoes.js";
 import initModal from "./modules/modal.js";
@@ -9,6 +8,7 @@ import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/fucionamento.js"
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import initAnimaScroll from "./modules/scroll-animacao.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
